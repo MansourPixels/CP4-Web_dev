7ea3f15cfbe4ad9e16bfd7247ddde2e82fa281cd
Henrique-CBueno <henriquecb2507@outlook.pt>
2026-09-13 16:36:48 -0300

    feat: add discovery search and shared navigation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import HomePage from './pages/HomePage'
+import SearchPage from './pages/SearchPage'
+import DetailPage from './pages/DetailPage'
+import ListPage from './pages/ListPage'
+import { useMediaLibrary } from './hooks/useMediaLibrary'
+
+export default function App() {
+  const library = useMediaLibrary()
+  return <Layout savedCount={library.items.length}><Routes><Route path="/" element={<HomePage library={library} />} /><Route path="/buscar" element={<SearchPage library={library} />} /><Route path="/:mediaType/:id" element={<DetailPage library={library} />} /><Route path="/minha-lista" element={<ListPage library={library} />} /><Route path="*" element={<Navigate to="/" replace />} /></Routes></Layout>
+}
